8729cf09b1d69d504de5ec0e356f754f4144c289
Joscha <joscha@plugh.de>
2025-03-02 18:18:05 +0100

    Set up @ alias for src/

--- showbits-thermal-printer-ui/vite.config.ts
+++ showbits-thermal-printer-ui/vite.config.ts
@@ -1,8 +1,12 @@
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
+  },
 });
