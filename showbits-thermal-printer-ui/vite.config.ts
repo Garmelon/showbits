ad028bc522d95088f8c329188138f8fa7b3c2354
Joscha <joscha@plugh.de>
2025-03-26 20:53:51 +0100

    Build photo.html with vite

--- showbits-thermal-printer-ui/vite.config.ts
+++ showbits-thermal-printer-ui/vite.config.ts
@@ -1,14 +1,25 @@
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "node:url";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+
+const root = dirname(fileURLToPath(import.meta.url));
+function path(to: string): string {
+  return resolve(root, to);
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: "",
+  build: {
+    rollupOptions: {
+      input: [path("index.html"), path("photo.html")],
+    },
+  },
   resolve: {
-    alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
+    alias: { "@": path("src") },
   },
   server: {
     proxy: { "/api": "http://localhost:8080" },
